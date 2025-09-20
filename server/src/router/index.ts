d1f867e1eb862e467dfe3baa60ca966f02738a78
Alfikri <alfzein19@gmail.com>
2025-09-20 18:53:35 +0700

    Fix : coorrected env setup

--- server/src/router/index.ts
+++ server/src/router/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import authRouter from "../modules/auth/auth.routes.js";
 import paymentsRouter from "../modules/payments/payments.routes.js";
 import analysisRouter from "../modules/analysis/analysis.routes.js";
+import cvRouter from "../modules/cv/cv.routes.js";
 
 const api = Router();
 api.use("/auth", authRouter);
