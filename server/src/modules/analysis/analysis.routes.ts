09551375bc69baceb551e1c328c37ff306e3b85f
Alfikri <alfzein19@gmail.com>
2025-09-20 18:29:36 +0700

    Feat: Added endpoint for guest to analyze CV

--- server/src/modules/analysis/analysis.routes.ts
+++ server/src/modules/analysis/analysis.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { protect } from "../../middlewares/auth.middleware.js"; // Our security guard
 import AnalysisController from "./analysis.controller.js";
+import upload from "../../middlewares/multer.middleware.js";
 
 const analysisRouter = Router();
 
@@ -9,6 +10,12 @@ analysisRouter.post(
   "/cv/:cvId", // The URL will include the ID of the CV to analyze
   protect, // Only logged-in users can access this
   AnalysisController.analyzeCv
+);
+
+analysisRouter.post(
+  "/guest/cv",
+  upload.single("cv"), // Use multer to handle the direct file upload
+  AnalysisController.analyzeGuestCv
 );
 
 export default analysisRouter;
